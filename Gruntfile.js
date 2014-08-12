152e934aafde4fe5040b8fb834f4e8331c0b4de5
AlexPikalov <alex.pikalov.khar@gmail.com>
2014-08-12 08:17:10 +0300

    Prepare dev grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,9 +25,59 @@ module.exports = function (grunt) {
                     'yui-io-mock.min.js': ['yui-io-mock.js']
                 }
             }
+        },
+        shell: {
+            'git.add-files': {
+                command: 'git add .'
+            },
+            'git.commit': {
+                command: function (message) {
+                    console.log('Creating commit... ', 'git commit -m ' + message);
+                    return 'git commit -m ' + message;
+                }
+            },
+            'git.tag': {
+                command: function (tag) {
+                    return 'git tag <%= tag %>';
+                }
+            },
+            'git.tag-push': {
+                command: function (tag) {
+                    return 'git push origin <%= tag %>'
+                }
+            }
+        },
+        replace: {
+            'version-bower': {
+
+            },
+            'version-sourcecode': {
+
+            },
+            'version-packagefile': {
+
+            }
         }
     });
 
-//    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.registerTask('set-version', []);
+
+        grunt.registerTask('commit', function (message) {
+            grunt.task.run('uglify:dist');
+            grunt.task.run('shell:git.add-files');
+            console.log('message', message);
+            grunt.task.run('shell:git.commit:"' + message + '"');
+        });
+
+        grunt.registerTask('release', function (version) {
+            grunt.task.run('replace:version-bower:<%= version %>');
+            grunt.task.run('replace:version-sourcecode:<%= version %>');
+            grunt.task.run('replace:version-packagefile:<%= version %>');
+            grunt.task.run('shell:git.tag:"v<%= verion %>"');
+            grunt.task.run('shell:git.tag-push:"v<%= verion %>"');
+        });
+
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-text-replace');
 };
